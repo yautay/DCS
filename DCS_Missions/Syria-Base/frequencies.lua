4b470ce7283c0e94da7d877a304385166c02d158
yautay <yautay@gmail.com>
2022-04-03 11:57:29 +0200

    mission start bot

---
+++ DCS_Missions/Syria-Base/frequencies.lua
@@ -0,0 +1,4 @@
+freq_awacs = 251
+freq_aar = 251
+freq_lso = 264
+freq_marshal = 305
---
+++ DCS_Missions/Syria-Base/mission_start_bot.lua
@@ -0,0 +1,5 @@
+require "frequencies"
+
+
+local txt = "Mission started " .. "\n freqs:" .. "\nAWACS: " .. freq_awacs .. "\nAAR:" .. freq_aar .. "\nMARSHAL:" .. freq_marshal .. "\nLSO:" .. freq_marshal 
+BotSay(txt)
