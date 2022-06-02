1f958170da12c5011bbf657f233be5f668aaf336
Michal Pielaszkiewicz <michal.pielaszkiewicz@komputronik.pl>
2022-06-02 19:21:46 +0200

    freqs refactor

--- DCS_Missions/Training Grounds/Gulf/Gulf-Base-SC/variables.lua
+++ DCS_Missions/Training Grounds/Gulf/Gulf-Base-SC/variables.lua
@@ -1,107 +1,61 @@
-function frequencies()
-    local frequencies_data = {
-        freq_flight = {242.00, "FLIGHTS Hi", "AM"},
-        freq_flight_lo = {128.5, "FLIGHTS Lo", "AM"},
-        freq_flight_fm = {41.9, "FLIGHTS", " FM"},
-        freq_awacs = {247.50, "AWACS", "AM"},
-        freq_aar = {243.25, "TANKERS", "AM"},
-        freq_lso = {270.00, "Airbos LSO", "AM"},
-        freq_marshal = {305.00, "Airbos Marshal", "AM"},
-        freq_al_minhad = {250.10, "Al Minhad Tower Hi", "AM"},
-        freq_al_minhad_lo = {118.55, "Al Minhad Tower Lo", "AM"},
-        freq_al_minhad_fm = {38.5, "Al Minhad Tower", "FM"},
-        freq_khasab = {250.00, "Khasab Tower", "AM"},
-        freq_sc = {127.50, "CVN71 Tower", "AM"},
-        freq_elint_hormuz_hi = {255.50, "ELINT Hi AM", "AM"},
-        freq_elint_hormuz_lo = {121.75, "ELINT Lo AM", "AM"},
-        freq_elint_hormuz_fm = {35.00, "ELINT FM", "FM"},
-        freq_atis_elint_hormuz_hi = {256.50, "ATIS Hi", "AM"},
-        freq_atis_elint_hormuz_lo = {122.75, "ATIS Lo", "AM"},
-        freq_atis_elint_hormuz_fm = {34.50, "ATIS FM", "FM"},
-        freq_sc = {127.50, "CVN71 Tower", "AM"},
-        guard_hi = {243.00, "Guard Freq Hi", "AM"},
-        guard_lo = {121.5, "Guard Freq Lo", "AM"}
-
-    }
-    return frequencies_data
-end
-
-function manual_ordered_frequencies()
-    local freqs = frequencies()
-    local ordered_frequencies_data = {
-        freqs.freq_flight,
-        freqs.freq_flight_lo,
-        freqs.freq_flight_fm,
-        freqs.freq_aar,
-        freqs.freq_awacs,
-        freqs.freq_sc,
-        freqs.freq_al_minhad,
-        freqs.freq_khasab,
-        freqs.freq_lso,
-        freqs.freq_marshal,
-        freqs.freq_atis_hi,
-        freqs.freq_atis_lo,
-        freqs.freq_atis_fm,
-        freqs.freq_elint_hi,
-        freqs.freq_elint_lo,
-        freqs.freq_elint_fm,
-        freqs.guard_hi,
-        freqs.guard_lo
-    }
-    return ordered_frequencies_data
-end
-
-function icls()
-    local icls_data = {
-        icls_sc = {1, "CVN", "ICLS CVN-71"},
-        ils_al_minhad090 = {110.70, "n/a", "ILS Al Minhad rwy 09"},
-        ils_al_minhad270 = {110.75, "n/a", "ILS Al Minhad rwy 27"},
-        ils_khasab194 = {110.75, "n/a", "ILS Khasab rwy 19"}
-    }
-    return icls_data
-end
-
-function manual_ordered_icls()
-    local icls = icls()
-    local ordered_icls = {
-        icls.icls_sc,
-        icls.ils_al_minhad090,
-        icls.ils_al_minhad270,
-        icls.ils_khasab194
-    }
-    return ordered_icls
-end
-
-function tacans()
-    local tacans_data = {
-        tacan_sc = {74, "X", "CVN", "CVN-71"},
-        tacan_arco = {1, "X", "RCV", "Recovery Tanker CVN-71"},
-        tacan_shell = {70, "X", "SHL", "Tanker Shell"},
-        tacan_al_minhad = {99, "X", "MIN", "Al Minhad AFB"}
-    }
-    return tacans_data
-end
-
-function manual_ordered_tacans()
-    local tcs = tacans()
-    local ordered_tcs = {
-        tcs.tacan_sc,
-        tcs.tacan_arco,
-        tcs.tacan_shell,
-        tcs.tacan_al_minhad,
-    }
-    return ordered_tcs
-end
-
-function air_routes()
-    local routes_data = {
-        {"Incirlik", 167, 100, "Al-Assad"},
-        {"Paphos", 79, 176, "Al-Assad"},
-        {"Paphos", 113, 132, "Beirut"},
-        {"Akrotiri", 74, 155, "Al-Assad"},
-        {"Ramat David", 15, 169, "Al-Assad"}
-    }
-    return routes_data
-end
-
-radio_frequencies = frequencies()
+frequencies_elint_data = {
+    freq_elint_hormuz_hi = {255.50, "Sector Hormuz Hi", "AM"},
+    freq_elint_hormuz_lo = {121.75, "Sector Hormuz Lo", "AM"},
+    freq_elint_hormuz_fm = {35.00, "Sector Hormuz FM", "FM"},
+    freq_elint_atis_hormuz_hi = {256.50, "Sector Hormuz ATIS Hi", "AM"},
+    freq_elint_atis_hormuz_lo = {122.75, "Sector Hormuz ATIS Lo", "AM"},
+    freq_elint_atis_hormuz_fm = {34.50, "Sector Hormuz ATIS FM", "FM"},
+    freq_elint_kish_hi = {255.50, "Sector Kish Hi", "AM"},
+    freq_elint_kish_lo = {121.75, "Sector Kish Lo", "AM"},
+    freq_elint_kish_fm = {35.00, "Sector Kish FM", "FM"},
+    freq_elint_atis_kish_hi = {256.50, "Sector Kish ATIS Hi", "AM"},
+    freq_elint_atis_kish_lo = {122.75, "Sector Kish ATIS Lo", "AM"},
+    freq_elint_atis_kish_fm = {34.50, "Sector Kish ATIS FM", "FM"}
+}
+frequencies_awacs_data = {
+    freq_awacs_darkstar = {247.50, "AWACS Darkstar", "AM"}
+    freq_awacs_wizard = {247.50, "AWACS Wizard", "AM"}
+}
+frequencies_aar_data = {
+    freq_aar_shell = {243.25, "TANKER Shell", "AM"},
+    freq_aar_arco = {243.25, "TANKER Arco", "AM"},
+}
+frequencies_flights_data = {
+    freq_flight_hi = {242.00, "FLIGHTS Hi", "AM"},
+    freq_flight_lo = {128.5, "FLIGHTS Lo", "AM"},
+    freq_flight_fm = {41.9, "FLIGHTS FM", " FM"},
+}
+frequencies_cv_data = {
+    freq_lso = {270.00, "Airbos LSO", "AM"},
+    freq_marshal = {305.00, "Airbos Marshal", "AM"}
+    freq_sc = {127.50, "CVN71 Tower", "AM"}
+}
+frequencies_ground_data = {
+    freq_al_minhad_hi = {250.10, "Al Minhad Tower Hi", "AM"},
+    freq_al_minhad_lo = {118.55, "Al Minhad Tower Lo", "AM"},
+    freq_al_minhad_fm = {38.5, "Al Minhad Tower", "FM"},
+    freq_khasab_hi = {250.00, "Khasab Tower", "AM"},   
+}
+frequencies_special_data = {
+    guard_hi = {243.00, "Guard Freq Hi", "AM"},
+    guard_lo = {121.5, "Guard Freq Lo", "AM"}
+}
+icls_data = {
+    icls_sc = {1, "CVN", "ICLS CVN-71"},
+    ils_al_minhad090 = {110.70, "n/a", "ILS Al Minhad rwy 09"},
+    ils_al_minhad270 = {110.75, "n/a", "ILS Al Minhad rwy 27"},
+    ils_khasab194 = {110.75, "n/a", "ILS Khasab rwy 19"}
+}
+tacan_data = {
+    tacan_sc = {74, "X", "CVN", "CVN-71"},
+    tacan_arco = {1, "X", "RCV", "Recovery Tanker CVN-71"},
+    tacan_shell = {70, "X", "SHL", "Tanker Shell"},
+    tacan_al_minhad = {99, "X", "MIN", "Al Minhad AFB"}
+}
+routes_data = {
+    {"Incirlik", 167, 100, "Al-Assad"},
+    {"Paphos", 79, 176, "Al-Assad"},
+    {"Paphos", 113, 132, "Beirut"},
+    {"Akrotiri", 74, 155, "Al-Assad"},
+    {"Ramat David", 15, 169, "Al-Assad"}
+}
