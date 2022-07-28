b71201ee350acd9c8e1efa34560dbb677d4a9f61
MichaU <yautay@gmail.com>
2022-07-28 22:57:58 +0200

    wip

--- DCS_Missions/DCS_Maps/Syria/Base/Scripts/4_1_range.lua
+++ DCS_Missions/DCS_Maps/Syria/Base/Scripts/4_1_range.lua
@@ -1,27 +1,68 @@
-local zone_range_hatay = ZONE_POLYGON:New("Range-1", GROUP:FindByName("Range-1")):DrawZone(-1, {1,0.7,0.1}, 1, {1,0.7,0.1}, 0.2, 0, true)
-
--- Strafe pits. Each pit can consist of multiple targets. Here we have two pits and each of the pits has two targets.
--- These are names of the corresponding units defined in the ME.
-local range_hatay_strafepit_west = {"Range-Hatay-Pit-W-1", "Range-Hatay-Pit-W-2"}
-local range_hatay_strafepit_east = {"Range-Hatay-Pit-E-1", "Range-Hatay-Pit-E-1"}
-
- -- Table of bombing target names. Again these are the names of the corresponding units as defined in the ME.
-local range_hatay_bombtargets = {"Range-Hatay-Bomb-S",
-                                 "Range-Hatay-Bomb-C",
-                                 "Range-Hatay-Bomb-N"}
- -- Create a range object.
-RangeHatay=RANGE:New("Hatay Range")
-
- -- Distance between strafe target and foul line. You have to specify the names of the unit or static objects.
- -- Note that this could also be done manually by simply measuring the distance between the target and the foul line in the ME.
-
- -- Add strafe pits. Each pit (left and right) consists of two targets. Where "nil" is used as input, the default value is used.
-RangeHatay:AddStrafePit(range_hatay_strafepit_west, 3000, 300, 200, false, 10, 700)
-RangeHatay:AddStrafePit(range_hatay_strafepit_east, 3000, 300, 200, false, 20, 700)
-
- -- Add bombing targets. A good hit is if the bomb falls less then 50 m from the target.
-RangeHatay:AddBombingTargets(range_hatay_bombtargets, 50)
-
- -- Start range.
 RANGE:SetAutosaveOn()
+
+function get_range_wx(ZoneObject)
+    local CoordinateObject = ZoneObject:GetCoordinate()
+    local pressure = CoordinateObject:GetPressureText()
+    local temperature = CoordinateObject:GetTemperatureText()
+    local wind_0 = CoordinateObject:GetWindText()
+    local wind_1km = CoordinateObject:GetWindText(1000)
+    local wind_3km = CoordinateObject:GetWindText(3000)
+    local wind_6km = CoordinateObject:GetWindText(6000)
+    local wx_data = {
+        ["pressure"] = pressure,
+        ["temperature"] = temperature,
+        ["wind_0"] = wind_0,
+        ["wind_1km"] = wind_1km,
+        ["wind_3km"] = wind_3km,
+        ["wind_6km"] = wind_6km,
+    }
+    BASE:E(wx_data)
+    return wx_data
+end
+
+function iterate_set(SetObject)
+    local set_data = {}
+    set_data["names"] = {}
+    SetObject:ForEachStatic(
+        function(GroupObject)
+            local object_name = GroupObject:GetName()
+            local object_coordinates = GroupObject:GetCoordinate()
+            local object_coordinates_lldms = object_coordinates:ToStringLLDMS()
+            local object_coordinates_llddm = object_coordinates:ToStringLLDDM()
+            local object_coordinate_mgrs = object_coordinates:ToStringMGRS()
+            local object_coordinates_h = object_coordinates:GetLandHeight()
+            set_data[object_name] = {
+                    ["lldms"] = object_coordinates_lldms,
+                    ["llddm"] = object_coordinates_llddm,
+                    ["mgrs"] = object_coordinate_mgrs,
+                    ["height_mtrs"] = object_coordinates_h,
+                    ["height_ft"] = UTILS.MetersToFeet(object_coordinates_h)
+            }
+            BASE:E(object_name)
+            table.insert(set_data["names"], object_name)
+        end)
+    return set_data
+end
+
+local RangeHatay=RANGE:New("Range Hatay")
+--FIX DRAWIG
+local zone_range_hatay = ZONE_POLYGON:New("Zone Range Hatay", GROUP:FindByName("Zone Range Hatay")):DrawZone(1, {1,0.7,0.1}, 1, {1,0.7,0.1}, 0.2, 0, true)
+
+local set_range_hatay_strafepits = SET_STATIC:New():FilterPrefixes("Range Hatay Target Pit"):FilterStart()
+local set_range_hatay_bombtargets = SET_STATIC:New():FilterPrefixes("Range Hatay Target Bomb"):FilterStart()
+
+local dict_range_hatay_strafepits = iterate_set(set_range_hatay_strafepits)
+local dict_hatay_bombtargets = iterate_set(set_range_hatay_bombtargets)
+--SAVE REPORT
+local dict_range_hatay_wx = get_range_wx(zone_range_hatay)
+
+--RANGE.AddStrafePitGroup(group, boxlength, boxwidth, heading, inverseheading, goodpass, foulline)
+RangeHatay:AddStrafePit(dict_range_hatay_strafepits.names, 3000, 500, 200, false, 10, 400)
+
+--RANGE.AddBombingTargetGroup(group, goodhitrange, randommove)
+BASE:E(dict_hatay_bombtargets.names)
+RangeHatay:AddBombingTargets(dict_hatay_bombtargets.names, 20)
+
+-- Start range.
 RangeHatay:Start()
+
--- DCS_Missions/DCS_Maps/Syria/Base/builder.py
+++ DCS_Missions/DCS_Maps/Syria/Base/builder.py
@@ -1,3 +1,5 @@
+import string
+
 from lib import *
 from DCS_Maps.Syria.Base.Scripts import *
 from collections import OrderedDict
@@ -6,6 +8,7 @@ import os
 CWD = os.path.dirname(os.path.abspath(__file__))
 FRAMEWORK_FILE = "frameworks.lua"
 SCRIPTS_FILE = "script.lua"
+DYNAMIC_LOAD_LUA = "dynamic_load.lua"
 
 frameworks_order = OrderedDict()
 frameworks_order["\n--1 - PATHS\n"] = PATH_SCRIPT_PATHS
@@ -30,10 +33,23 @@ scripts_order["\n--4.2 - CSAR\n"] = PATH_SCRIPT_CSAR
 # scripts_order["\n--11.2 - AW KUT\n"] = PATH_SCRIPT_AW_KUTAISI
 # scripts_order["\n--13 - SCHEDULER\n"] = PATH_SCRIPT_SCHEDULERS
 
+content_scripts = ""
+content_frameworks = ""
+
 
 def open_script(path):
     with open(path, "rb") as file:
         return file.read()
+
+
+def read_lines(path):
+    with open(path, "r") as file:
+        return file.readlines()
+
+
+def write_lines(path, lines):
+    with open(path, "w") as file:
+        file.writelines(lines)
 
 
 def append_script(path, content):
@@ -53,7 +69,26 @@ for file in [FRAMEWORK_FILE, SCRIPTS_FILE]:
 for k, v in frameworks_order.items():
     append_script(FRAMEWORK_FILE, k)
     append_binary(FRAMEWORK_FILE, open_script(v))
+    content_frameworks += '"{}", '.format(v.replace("\\", "\\\\"))
 
 for k, v in scripts_order.items():
     append_script(SCRIPTS_FILE, k)
     append_binary(SCRIPTS_FILE, open_script(v))
+    content_scripts += '"{}", '.format(v.replace("\\", "\\\\"))
+
+content_scripts = "{" + content_scripts + "}"
+content_frameworks = "{" + content_frameworks + "}"
+
+dynamic_load_scripts = f"local SCRIPTS = {content_scripts}\n"
+dynamic_load_frameworks = f"local FRAMEWORKS = {content_frameworks}\n"
+
+template = read_lines("dynamic_load.template")
+
+for i in range(len(template)):
+    if template[i].__contains__("FRAMEWORKS_PLACEHOLDER"):
+        template[i] = dynamic_load_frameworks
+    elif template[i].__contains__("SCRIPTS_PLACEHOLDER"):
+        template[i] = dynamic_load_scripts
+
+write_lines(DYNAMIC_LOAD_LUA, template)
+# append_script(DYNAMIC_LOAD_LUA, dynamic_load_lua)
