5fe52c0c0294e6b9b8b4452ece4bc533f77ba2c2
Michał Pielaszkiewicz <m.pielaszkiewicz@komputronik.pl>
2022-12-30 00:02:55 +0100

    init

--- DCS_Missions/lib/__init__.py
+++ DCS_Missions/lib/__init__.py
@@ -1,9 +1,7 @@
 import os
 
-PATH_LIBS = os.path.dirname(os.path.abspath(__file__))
 
-PATH_LIBS_MOOSE = os.path.join(PATH_LIBS, "Moose.lua")
-PATH_LIBS_MIST = os.path.join(PATH_LIBS, "mist_4_5_107.lua")
-PATH_LIBS_HOUND = os.path.join(PATH_LIBS, "HoundElint.lua")
-PATH_LIBS_STTS = os.path.join(PATH_LIBS, "DCS-SimpleTextToSpeech.lua")
-PATH_LIBS_SKYNET = os.path.join(PATH_LIBS, "skynet-iads_3_0_3.lua")
+PATH_LIB = os.path.dirname(os.path.abspath(__file__))
+
+PATH_LIBS_MOOSE = os.path.join(PATH_LIB, "Moose.lua")
+PATH_LIBS_STTS = os.path.join(PATH_LIB, "DCS-SimpleTextToSpeech.lua")
