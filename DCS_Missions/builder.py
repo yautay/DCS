3d1703dadc79bb34dac54845ffde391c713e9586
yautay <yautay@gmail.com>
2023-02-25 17:14:04 +0100

    clean master

--- DCS_Missions/builder.py
+++
@@ -1,7 +0,0 @@
-import os
-
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-print("Root dir", ROOT_DIR)
-
-
--- DCS_Missions/lib/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-PATH_LIB = os.path.dirname(os.path.abspath(__file__))
-
-PATH_LIBS_MOOSE = os.path.join(PATH_LIB, "Moose.lua")
-PATH_LIBS_STTS = os.path.join(PATH_LIB, "DCS-SimpleTextToSpeech.lua")
