dc488dd97fae37fd6bd50602be3c94c712920e62
yautay <yautay>
2024-07-23 14:21:58 +0200

    senaki strike added mig21 added sam range reworked

--- DCS_Missions/DCS_Maps/Kavkaz/PFS_AG_Range/Scripts/3_3_RANGES.lua
+++ DCS_Missions/DCS_Maps/Kavkaz/PFS_AG_Range/Scripts/3_3_RANGES.lua
@@ -22,33 +22,6 @@ RangeObject_IRON:SetAutosaveOn()
 RangeObject_IRON:SetMessageTimeDuration(5)
 RangeObject_IRON:SetFunkManOn(10042, "127.0.0.1")
 
-function spawnSenakiRange()
-    RangeObject_IRON:Start()
-    marker_bomb = MARKER:New( GROUP:FindByName("G_TARGET_BMB"):GetCoordinate(), "Iron Bombs Target"):ToCoalition( coalition.side.BLUE )
-    marker_strafe = MARKER:New( GROUP:FindByName("G_TARGET_STR"):GetCoordinate(), "Strafe Pit - ingress from East!!"):ToCoalition( coalition.side.BLUE )
-end
-
-ZoneRangeSAM = ZONE:New("RANGE-SAM")
-Fox_SAM=FOX:New()
-Zone_SA3LZ = ZONE:New("SA3 LZ")
-Zone_SA2LZ = ZONE:New("SA2 LZ")
-Zone_SA6LZ = ZONE:New("SA6 LZ")
-Fox_SAM:AddSafeZone(ZoneRangeSAM)
-Fox_SAM:AddLaunchZone(Zone_SA3LZ)
-Fox_SAM:AddLaunchZone(Zone_SA2LZ)
-Fox_SAM:AddLaunchZone(Zone_SA6LZ)
-
-function spawnSamRange()
-    Fox_SAM:Start()
-    Spawn_SAMSA2 = SPAWN:New("Red SAM SA-2"):InitLimit( 15, 15 ):Spawn()
-    Spawn_SAMSA3 = SPAWN:New("Red SAM SA-3"):InitLimit( 13, 13 ):Spawn()
-    Spawn_SAMSA6 = SPAWN:New("Red SAM SA-6"):InitLimit( 11, 11 ):Spawn()
-    marker_SA3 = MARKER:New( Zone_SA3LZ:GetCoordinate(), "SA-3 Range | S-125 Low Blow | P19 Flat Face"):ToCoalition( coalition.side.BLUE )
-    marker_SA2 = MARKER:New( Zone_SA2LZ:GetCoordinate(), "SA-2 Range | S-75 Fan Song | P19 Flat Face"):ToCoalition( coalition.side.BLUE )
-    marker_SA6 = MARKER:New( Zone_SA6LZ:GetCoordinate(), "SA-6 Range | Straight Flush"):ToCoalition( coalition.side.BLUE )
-end
-
-MenuSpawnAG = MENU_COALITION:New( coalition.side.BLUE, "Spawn Menu AG" )
-
-MenuSpawnSenakiRange = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn Senaki Range", MenuSpawnAG, spawnSenakiRange )
-MenuSpawnSamRange = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn SAM Range", MenuSpawnAG, spawnSamRange )
+RangeObject_IRON:Start()
+marker_bomb = MARKER:New( GROUP:FindByName("G_TARGET_BMB"):GetCoordinate(), "Iron Bombs Target"):ToCoalition( coalition.side.BLUE )
+marker_strafe = MARKER:New( GROUP:FindByName("G_TARGET_STR"):GetCoordinate(), "Strafe Pit - ingress from East!!"):ToCoalition( coalition.side.BLUE )
--- DCS_Missions/DCS_Maps/Kavkaz/PFS_AG_Range/Scripts/4_4_SPAWN.lua
+++ DCS_Missions/DCS_Maps/Kavkaz/PFS_AG_Range/Scripts/4_4_SPAWN.lua
@@ -1,33 +1,33 @@
-Spawn_Shell1 = SPAWN:New("SHELL #1"):InitLimit( 1, 0 ):SpawnScheduled( 60, .1 ):OnSpawnGroup( function (shell_11) shell_11:CommandSetCallsign(CALLSIGN.Tanker.Shell, 1) end):InitRepeatOnLanding()
-Spawn_Texaco_1 = SPAWN:New("TEXACO #1"):InitLimit( 1, 0 ):SpawnScheduled( 60, .1 ):OnSpawnGroup( function (texaco_11) texaco_11:CommandSetCallsign(CALLSIGN.Tanker.Texaco, 1) end):InitRepeatOnLanding()
-
-
-
-function spawnStrike()
-    Spawn_Aggressor_1 = SPAWN:New("AGRESOR1"):Spawn()
-    Spawn_Aggressor_2 = SPAWN:New("AGRESOR2"):Spawn()
-    Spawn_Aggressor_3 = SPAWN:New("AGRESOR3"):Spawn()
-    Spawn_Aggressor_4 = SPAWN:New("LVL BOMBERS#1"):Spawn()
-    Spawn_Aggressor_5 = SPAWN:New("LVL BOMBERS#2"):Spawn()
-end
-
-function spawnMig21()
-    Spawn_Mig21 = SPAWN:New("MIGS21"):Spawn()
-end
-function spawnMig19()
-    Spawn_Mig21 = SPAWN:New("MIGS19"):Spawn()
-end
-function spawnMig15()
-    Spawn_Mig21 = SPAWN:New("MIGS15"):Spawn()
-end
-function spawnMig29()
-    Spawn_Mig21 = SPAWN:New("MIGS15"):Spawn()
-end
-
-MenuSpawnAir = MENU_COALITION:New( coalition.side.BLUE, "Spawn Menu PvE" )
-
-MenuSpawnAttackOnSenaki = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn Strike", MenuSpawnAir, spawnStrike )
-MenuSpawnMIG21 = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn 2-ship of MiG-21 from South", MenuSpawnAir, spawnMig21 )
-MenuSpawnMIG19 = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn 2-ship of MiG-19 from South-East", MenuSpawnAir, spawnMig19 )
-MenuSpawnMIG15 = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn 4-ship of MiG-15 from East", MenuSpawnAir, spawnMig15 )
-MenuSpawnMIG29 = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn 2-ship of MiG-29 from West", MenuSpawnAir, spawnMig29 )
+--Spawn_Shell1 = SPAWN:New("SHELL #1"):InitLimit( 1, 0 ):SpawnScheduled( 60, .1 ):OnSpawnGroup( function (shell_11) shell_11:CommandSetCallsign(CALLSIGN.Tanker.Shell, 1) end):InitRepeatOnLanding()
+--Spawn_Texaco_1 = SPAWN:New("TEXACO #1"):InitLimit( 1, 0 ):SpawnScheduled( 60, .1 ):OnSpawnGroup( function (texaco_11) texaco_11:CommandSetCallsign(CALLSIGN.Tanker.Texaco, 1) end):InitRepeatOnLanding()
+--
+--
+--
+--function spawnStrike()
+--    Spawn_Aggressor_1 = SPAWN:New("AGRESOR1"):Spawn()
+--    Spawn_Aggressor_2 = SPAWN:New("AGRESOR2"):Spawn()
+--    Spawn_Aggressor_3 = SPAWN:New("AGRESOR3"):Spawn()
+--    Spawn_Aggressor_4 = SPAWN:New("LVL BOMBERS#1"):Spawn()
+--    Spawn_Aggressor_5 = SPAWN:New("LVL BOMBERS#2"):Spawn()
+--end
+--
+--function spawnMig21()
+--    Spawn_Mig21 = SPAWN:New("MIGS21"):Spawn()
+--end
+--function spawnMig19()
+--    Spawn_Mig21 = SPAWN:New("MIGS19"):Spawn()
+--end
+--function spawnMig15()
+--    Spawn_Mig21 = SPAWN:New("MIGS15"):Spawn()
+--end
+--function spawnMig29()
+--    Spawn_Mig21 = SPAWN:New("MIGS15"):Spawn()
+--end
+--
+--MenuSpawnAir = MENU_COALITION:New( coalition.side.BLUE, "Spawn Menu PvE" )
+--
+--MenuSpawnAttackOnSenaki = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn Strike", MenuSpawnAir, spawnStrike )
+--MenuSpawnMIG21 = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn 2-ship of MiG-21 from South", MenuSpawnAir, spawnMig21 )
+--MenuSpawnMIG19 = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn 2-ship of MiG-19 from South-East", MenuSpawnAir, spawnMig19 )
+--MenuSpawnMIG15 = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn 4-ship of MiG-15 from East", MenuSpawnAir, spawnMig15 )
+--MenuSpawnMIG29 = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn 2-ship of MiG-29 from West", MenuSpawnAir, spawnMig29 )
