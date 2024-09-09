76dd5fc045a91ae37cb9a882fe05f0ed88ab67bf
yautya@gmail.com <yautya@gmail.com>
2024-09-09 20:45:39 +0200

    wip

---
+++ DCS_Missions/DCS_Maps/Kavkaz/308SEAD/Scripts/paths.lua
@@ -0,0 +1,41 @@
+SERVER = true
+
+SERVER_DCS_PATH = "D:\\repo\\DCS\\DCS_Missions"
+LOCAL_DCS_PATH = "F:\\repo\\DCS\\DCS_Missions"
+
+SCRIPTS_PATH = "\\DCS_Maps\\Kavkaz\\308SEAD\\Scripts\\"
+
+LIBS_PATH = "\\lib"
+
+
+SRS_PATH = nil
+SRS_PORT = nil
+
+SRS_SERVER_PATH = "C:\\DCS-SimpleRadio-Standalone"
+SRS_SERVER_PORT = 5002
+SRS_LOCAL_PATH = "D:\\DCS-SimpleRadio-Standalone"
+SRS_LOCAL_PORT = 5002
+
+SERVER_SAVE_SHEET_PATH = "C:\\DCS_Data"
+LOCAL_SAVE_SHEET_PATH = "D:\\Data"
+
+if (SERVER) then
+	scripts_path = SERVER_DCS_PATH .. SCRIPTS_PATH
+	lib_path = SERVER_DCS_PATH .. LIBS_PATH
+	SRS_PATH = SRS_SERVER_PATH
+	SRS_PORT = SRS_SERVER_PORT
+	SHEET_PATH = SERVER_SAVE_SHEET_PATH
+else
+	scripts_path = LOCAL_DCS_PATH .. SCRIPTS_PATH
+	lib_path = LOCAL_DCS_PATH .. LIBS_PATH
+	SRS_PATH = SRS_LOCAL_PATH
+	SRS_PORT = SRS_LOCAL_PORT
+	SHEET_PATH = LOCAL_SAVE_SHEET_PATH
+end
+
+env.info("CUSTOM ### PATH SETUP ###")
+env.info(string.format("CUSTOM SERVER->%s", tostring(SERVER)))
+env.info(string.format("CUSTOM SCRIPTS PATH->%s", scripts_path))
+env.info(string.format("CUSTOM SRS PATH->%s", SRS_PATH))
+env.info(string.format("CUSTOM SRS PORT->%s", SRS_PORT))
+env.info("CUSTOM ### PATH SETUP ###")
--- DCS_Missions/lib/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-PATH_LIBS = os.path.dirname(os.path.abspath(__file__))
-
-PATH_LIBS_MOOSE = os.path.join(PATH_LIBS, "Moose.lua")
-PATH_LIBS_MOOSE_CUSTOM = os.path.join(PATH_LIBS, "MooseCustom.lua")
-PATH_LIBS_STTS = os.path.join(PATH_LIBS, "DCS-SimpleTextToSpeech.lua")
