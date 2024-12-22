a463adf0e15be8384febd7e80a222c1b566b6aeb
cpt_sztos <yautay@gmail.com>
2024-12-22 18:47:59 +0100

    308 PFS Range

--- DCS_Missions/DCS_Maps/Normandy/m1/Scripts/NO_6_1_PVE_Trainer.lua
+++ DCS_Missions/DCS_Maps/Normandy/m1/Scripts/NO_6_1_PVE_Trainer.lua
@@ -1,24 +1,28 @@
-ZONE_SPAWN_1 = ZONE:New("SPAWN-1")
-ZONE_SPAWN_2 = ZONE:New("SPAWN-2")
-ZONE_SPAWN_3 = ZONE:New("SPAWN-3")
-ZONE_SPAWN_4 = ZONE:New("SPAWN-4")
-ZONE_SPAWN_5 = ZONE:New("SPAWN-5")
-ZONE_SPAWN_6 = ZONE:New("SPAWN-6")
-SPAWN_ZONES = {ZONE_SPAWN_1, ZONE_SPAWN_2, ZONE_SPAWN_3, ZONE_SPAWN_4, ZONE_SPAWN_5, ZONE_SPAWN_6}
+TEMPLATE_JU88 = "TEMPLATE JU88"
 
-TEMPLATE_A20 = "TMP-A20"
-TEMPLATE_MUSTANG = "TMP-MUSTANG"
-TEMPLATE_MOSQUITO = "TMP-MOSQUITO"
-TEMPLATE_SPITFIRE = "TMP-SPITFIRE"
-TEMPLATE_YAK = "TMP-YAK"
+MenuPVE_Blue = MENU_COALITION:New(coalition.side.BLUE, "PVE Trainer")
 
-MenuPVE = MENU_COALITION:New(coalition.side.BLUE, "PVE Trainer")
-MenuPVE_A20 = MENU_COALITION:New(coalition.side.BLUE, "A20", MenuPVE)
-MenuPVE_P51 = MENU_COALITION:New(coalition.side.BLUE, "P51", MenuPVE)
-MenuPVE_SPIT = MENU_COALITION:New(coalition.side.BLUE, "SPITFIRE", MenuPVE)
-MenuPVE_MOSIE = MENU_COALITION:New(coalition.side.BLUE, "MOSIE", MenuPVE)
-MenuPVE_YAK = MENU_COALITION:New(coalition.side.BLUE, "YAK", MenuPVE)
+MenuPVE_Aspect = MENU_COALITION:New(coalition.side.BLUE, "Aspect", MenuPVE_Blue)
 
+MenuPVE_Altitude = MENU_COALITION:New(coalition.side.BLUE, "Altitude", MenuPVE_Blue)
+
+MenuPVE_Bearing = MENU_COALITION:New(coalition.side.BLUE, "Bearing", MenuPVE_Blue)
+
+
+MenuPVE_Ju88 = MENU_COALITION:New(coalition.side.BLUE, "Ju-88", MenuPVE)
+
+local function set_altitude()
+    local spawned_group = SPAWN
+        :New(template_name)
+        :InitRandomizeZones( SPAWN_ZONES )
+        :Spawn()
+    local dest = ZONE_ENGAGE:GetVec2()
+    local coord_dest = COORDINATE:NewFromVec2(dest, UTILS.FeetToMeters(15000))
+    spawned_group:RouteAirTo(coord_dest, COORDINATE.WaypointAltType.BARO, COORDINATE.WaypointType.TurningPoint, COORDINATE.WaypointAction.FlyoverPoint, UTILS.KnotsToKmph(250))
+    spawned_group:EnRouteTaskEngageTargetsInZone(dest, UTILS.NMToMeters(60))
+    local msg = template_name .. " SPAWNED!"
+    msgToAll({msg, 3})
+end
 
 local function Spawn_Group(template_name)
     local spawned_group = SPAWN
