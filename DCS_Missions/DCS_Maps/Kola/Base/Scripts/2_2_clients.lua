cb12045829cb4a3ba23e05fccba2d8398f090f02
m.pielaszkiewicz <m.pielaszkiewicz@komputronik.pl>
2025-05-07 11:53:25 +0200

    wip

--- DCS_Missions/DCS_Maps/Kola/Base/Scripts/2_2_clients.lua
+++ DCS_Missions/DCS_Maps/Kola/Base/Scripts/2_2_clients.lua
@@ -1,17 +1,31 @@
-ClientSet = SET_CLIENT:New():FilterOnce()
+allPlayers = SET_PLAYER:New():FilterCoalitions("blue"):FilterActive():FilterStart()
 
-function SetEventHandler()
-    ClientBirth = ClientSet:HandleEvent(EVENTS.PlayerEnterAircraft)
+allPlayers:ForEachPlayer(function(player)
+    player:AddCommand("GPS Position", nil, function()
+        local unit = player:GetUnit()
+        if unit then
+        local pos = unit:GetPointVec3()
+        local lat, lon, alt_meters = coord.LOtoLL(pos)
+        local alt_feet = alt_meters * 3.28084 -- metry na stopy
+
+        local velocity = unit:GetVelocity()
+        local speed_mps = math.sqrt(velocity.x^2 + velocity.y^2 + velocity.z^2)
+        local speed_knots = speed_mps * 1.94384 -- m/s na węzły
+
+        local heading_rad = math.atan2(velocity.x, velocity.z)
+        local heading_deg = math.deg(heading_rad)
+        if heading_deg < 0 then heading_deg = heading_deg + 360 end -- normalize do 0-360
+
+        local messageText = string.format(
+            "POS: %.5f°N, %.5f°E\n" ..
+            "ALT: %.1f ft\n" ..
+            "SOG: %.1f knots\n" ..
+            "COG: %.1f°",
+            lat, lon, alt_feet, speed_knots, heading_deg
+        )
+
+        MESSAGE:New(messageText, 15):ToPlayer(myPlayer)
+    else
+        MESSAGE:New("GPS SYSTEM ERROR", 5):ToPlayer(myPlayer)
+    end
 end
-
-function ClientSet:OnEventPlayerEnterAircraft(event_data)
-    local unit_name = event_data.IniUnitName
-    local group = event_data.IniGroup
-    local player_name = event_data.IniPlayerName
-    local client = "Pilot " .. player_name .. " to " .. unit_name .. " Connected!"
-    env.info(client)
-end
-
-SetEventHandler()
-
-MenuPyrlandia = MENU_COALITION:New( coalition.side.BLUE, "PYRLANDIA" )
--- DCS_Missions/DCS_Maps/Kola/Base/Scripts/3_3_RANGES.lua
+++ DCS_Missions/DCS_Maps/Kola/Base/Scripts/3_3_RANGES.lua
@@ -1,57 +1,57 @@
-RangeAG = RANGE:New("Range AG")
-ZoneRangeAG = ZONE:New("RANGE AG"):DrawZone(2, CONST.RGB.zone_red, 1, CONST.RGB.zone_red, .3, 1, true)
-RangeAG:SetRangeZone(ZoneRangeAG)
-
-BombTargets = { "Range AG Bomb" }
-StrafeTargets = { "Range AG Strafe" }
-
-RangeAG:AddBombingTargets(BombTargets, 20, false)
-
-local boxlength = UTILS.NMToMeters(3)
-local boxwidth = UTILS.NMToMeters(1)
-local heading = 150
-local foulline = 150
-
---Base:AddStrafePit(targetnames, boxlength, boxwidth, heading, inverseheading, goodpass, foulline)
-RangeAG:AddStrafePit(StrafeTargets, boxlength, boxwidth, heading, false, 10, foulline)
-
--- Start range.
-RangeAG:SetDefaultPlayerSmokeBomb(false)
-RangeAG:SetTargetSheet(SHEET_PATH, "Range-")
-RangeAG:SetAutosaveOn()
-RangeAG:SetMessageTimeDuration(5)
-RangeAG:Start()
-
-function targets_coordinates(list_targets_names)
-    local tgts_tbl = {}
-    for index, value in ipairs(list_targets_names) do
-        local unit = STATIC:FindByName(value)
-        local unit_type = unit:GetTypeName()
-        local coords = unit:GetCoordinate()
-        local mgrs = coords:ToStringMGRS()
-        local lldm = coords:ToStringLLDDM()
-        table.insert(tgts_tbl, lldm .. "   " .. mgrs .. "\n")
-    end
-    return tgts_tbl
-end
-
-function range_report(range_object, table_bomb_targets, table_strafe_targets)
-    local name = range_object.rangename
-    local bomb_tgts = targets_coordinates(table_bomb_targets)
-    local strafe_tgts = targets_coordinates(table_strafe_targets)
-
-    local range_report = {}
-    table.insert(range_report, os.date('%Y-%m-%d/%H%ML') .. "\n")
-    table.insert(range_report, name .. "\n")
-    table.insert(range_report, "BOMB TARGETS\n")
-    for index, value in ipairs(bomb_tgts) do
-        table.insert(range_report, value)
-    end
-    table.insert(range_report, "STRAFE TARGETS\n")
-    for index, value in ipairs(strafe_tgts) do
-        table.insert(range_report, value)
-    end
-    saveToFile(SHEET_PATH .. "\\NOTAM-RANGE-" .. string.upper(name), table.concat(range_report))
-end
-
-range_report(RangeAG, BombTargets, StrafeTargets)
+-- RangeAG = RANGE:New("Range AG")
+-- ZoneRangeAG = ZONE:New("RANGE AG"):DrawZone(2, CONST.RGB.zone_red, 1, CONST.RGB.zone_red, .3, 1, true)
+-- RangeAG:SetRangeZone(ZoneRangeAG)
+--
+-- BombTargets = { "Range AG Bomb" }
+-- StrafeTargets = { "Range AG Strafe" }
+--
+-- RangeAG:AddBombingTargets(BombTargets, 20, false)
+--
+-- local boxlength = UTILS.NMToMeters(3)
+-- local boxwidth = UTILS.NMToMeters(1)
+-- local heading = 150
+-- local foulline = 150
+--
+-- --Base:AddStrafePit(targetnames, boxlength, boxwidth, heading, inverseheading, goodpass, foulline)
+-- RangeAG:AddStrafePit(StrafeTargets, boxlength, boxwidth, heading, false, 10, foulline)
+--
+-- -- Start range.
+-- RangeAG:SetDefaultPlayerSmokeBomb(false)
+-- RangeAG:SetTargetSheet(SHEET_PATH, "Range-")
+-- RangeAG:SetAutosaveOn()
+-- RangeAG:SetMessageTimeDuration(5)
+-- RangeAG:Start()
+--
+-- function targets_coordinates(list_targets_names)
+--     local tgts_tbl = {}
+--     for index, value in ipairs(list_targets_names) do
+--         local unit = STATIC:FindByName(value)
+--         local unit_type = unit:GetTypeName()
+--         local coords = unit:GetCoordinate()
+--         local mgrs = coords:ToStringMGRS()
+--         local lldm = coords:ToStringLLDDM()
+--         table.insert(tgts_tbl, lldm .. "   " .. mgrs .. "\n")
+--     end
+--     return tgts_tbl
+-- end
+--
+-- function range_report(range_object, table_bomb_targets, table_strafe_targets)
+--     local name = range_object.rangename
+--     local bomb_tgts = targets_coordinates(table_bomb_targets)
+--     local strafe_tgts = targets_coordinates(table_strafe_targets)
+--
+--     local range_report = {}
+--     table.insert(range_report, os.date('%Y-%m-%d/%H%ML') .. "\n")
+--     table.insert(range_report, name .. "\n")
+--     table.insert(range_report, "BOMB TARGETS\n")
+--     for index, value in ipairs(bomb_tgts) do
+--         table.insert(range_report, value)
+--     end
+--     table.insert(range_report, "STRAFE TARGETS\n")
+--     for index, value in ipairs(strafe_tgts) do
+--         table.insert(range_report, value)
+--     end
+--     saveToFile(SHEET_PATH .. "\\NOTAM-RANGE-" .. string.upper(name), table.concat(range_report))
+-- end
+--
+-- range_report(RangeAG, BombTargets, StrafeTargets)
