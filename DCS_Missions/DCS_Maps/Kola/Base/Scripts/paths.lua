389fdee6c214f70f3ac54cb5496ab65bc15bb0f7
yauta <cpt_sztos@example.com>
2025-06-02 23:06:36 +0200

    wip

--- DCS_Missions/DCS_Maps/Kola/Base/Scripts/paths.lua
+++ DCS_Missions/DCS_Maps/Kola/Base/Scripts/paths.lua
@@ -13,11 +13,11 @@ SRS_PORT = nil
 
 SRS_SERVER_PATH = "C:\\DCS-SimpleRadio-Standalone"
 SRS_SERVER_PORT = 5002
-SRS_LOCAL_PATH = "D:\\DCS-SimpleRadio-Standalone"
+SRS_LOCAL_PATH = "Z:\\DCS-SimpleRadio-Standalone"
 SRS_LOCAL_PORT = 5002
 
 SERVER_SAVE_SHEET_PATH = "C:\\DCS_Data"
-LOCAL_SAVE_SHEET_PATH = "D:\\Data"
+LOCAL_SAVE_SHEET_PATH = "Z:\\DUMP"
 
 if (SERVER) then
 	scripts_path = SERVER_DCS_PATH .. SCRIPTS_PATH
