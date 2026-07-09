146193932c9edf1f47516ea286b3103472c7dcd0
yautay <cpt_sztos@example.com>
2026-07-09 16:12:17 +0200

    wip

--- DCS_Missions/DCS_Maps/Normandy/op_jericho/builder.py
+++ DCS_Missions/DCS_Maps/Normandy/op_jericho/builder.py
@@ -1,5 +1,5 @@
 from DCS_Missions.lib import *
-from DCS_Missions.DCS_Maps.Normandy.m1.Scripts import *
+from DCS_Missions.DCS_Maps.Normandy.op_jericho.Scripts import *
 from DCS_Missions.lib import *
 from collections import OrderedDict
 import os
